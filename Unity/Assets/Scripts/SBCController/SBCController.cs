34a0e874d2d40ee215284e3d2fcdd46399afca11
darkfall <darkfall@gamemastercn.com>
2014-05-22 19:23:56 -0400

    add LED tutorial

--- Unity/Assets/Scripts/SBCController/SBCController.cs
+++ Unity/Assets/Scripts/SBCController/SBCController.cs
@@ -33,8 +33,8 @@ public class SBCController: TcpConnectionHandler {
 	public SBCPacketButton buttonStates;
 	public SBCPacketButton buttonStatesCache;
 	public SBCPacketJoystick joystickStates;
-    public bool connected = false;
-public string serverBinaryDir = "Mechgic/SBCController/";
+  public bool connected = false;
+  public string serverBinaryDir = "Mechgic/SBCController/";
 
 	Process serverProcess = null;
 	int receivedSBCButtonUpdate = 0;
